a75a6e3e2726e6e9a9068ea9b8f4d334501d456f
VasylIshchuk <studyvasylishchuk1@gmail.com>
2026-01-31 22:24:55 +0100

    Move frontend files to frontend/ subdirectory

--- src/components/shared/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from "lucide-react";
-
-export const Spinner = () => {
-  return (
-    <div className="flex w-full flex-1 items-center justify-center py-12">
-      <Loader2 className="h-10 w-10 animate-spin text-blue-600" />
-    </div>
-  );
-};
